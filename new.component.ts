f065303fb399546b3964bd3e0a36a95297e7a2a9
eZ0 <kusksu@gmail.com>
2018-09-20 15:28:05 +0200

    New component

---
+++ new.component.ts
@@ -0,0 +1,35 @@
+import {Component, Input, OnChanges, SimpleChanges} from "@angular/core";
+
+
+@Component({
+    selector: "vb-information-bar",
+    styles: [require('./vb-information-bar.component.scss')],
+    template: `
+
+    <div *ngIf="hasLimitedUsageWarning" class="__content h-flex-row h-flex-space-between h-flex-align-center h-padding-double">
+        <p class="__text">{{ limitedUsageTranslationKey | translate }}</p>
+        <button class="__button vb-icon-button vb-icon-button--close h-margin-left-double" (click)="showWarning()">
+            <i class="mdi mdi-close" ></i>
+        </button>
+        <p>{{amountOfWarnings}}</p>
+    </div>
+    
+`
+})
+
+export class InformationBar implements OnChanges {
+    @Input() public limitedUsageTranslationKey: string = null;
+    @Input() public amountOfWarnings: number = 0;
+
+    public hasLimitedUsageWarning = false;
+
+    public ngOnChanges(changes: SimpleChanges): void {
+        if (changes.hasOwnProperty('limitedUsageTranslationKey')) {
+            this.hasLimitedUsageWarning = changes['limitedUsageTranslationKey'].currentValue !== null;
+        }
+    }
+
+    public showWarning(): boolean {
+        this.hasLimitedUsageWarning = !this.hasLimitedUsageWarning;
+    }
+}
